9dd7f982563f1eb9c74f47de8eda8c639dc470fc
Yosuke <y.seki@bravesoft.co.jp>
2019-09-04 15:31:09 +0900

    add files

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 関　洋輔 on 2019/09/04.
+//  Copyright © 2019 関　洋輔. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController,YSRadioButtonViewControllerDelegate {
+
+    @IBOutlet weak var container: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let radio = YSRadioButtonViewController(labels: ["Orange","GrapeFruits","banana"])
+        radio.delegate = self
+        radio.labelColor = .darkGray
+        radio.labelMargin = 0
+        radio.lineWidth = 1
+        radio.radioHeadMargin = 0
+        radio.radioHeadStroke = .lightGray
+        radio.radioHeadFill = .red
+        addChild(radio)
+        radio.view.frame = container.bounds
+        container.addSubview(radio.view)
+        radio.didMove(toParent: self)
+    }
+    
+    func didYSRadioButtonSelect(no: Int) {
+        print(no)
+    }
+
+
+}
+
---
+++ YSRadioButton/YSRadioButton.h
@@ -0,0 +1,19 @@
+//
+//  YSRadioButton.h
+//  YSRadioButton
+//
+//  Created by 関　洋輔 on 2019/09/04.
+//  Copyright © 2019 関　洋輔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YSRadioButton.
+FOUNDATION_EXPORT double YSRadioButtonVersionNumber;
+
+//! Project version string for YSRadioButton.
+FOUNDATION_EXPORT const unsigned char YSRadioButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YSRadioButton/PublicHeader.h>
+
+
